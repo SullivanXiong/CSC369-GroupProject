84bcb4ca368c52618fdbbfbdc7228bfccc36534e
Sullivan Xiong <sxiong5307@gmail.com>
2021-03-18 10:20:00 -0700

    added query2, fixed up output formatting, pasted case fatality ratio based on long/lat back in

--- Project/src/main/scala/Project.scala
+++ Project/src/main/scala/Project.scala
@@ -117,8 +117,8 @@ object project {
   }
 
   def main(args: Array[String]): Unit = {
-    if (args.length != 4) {
-      println("Usage: App [vaccinationsFile] [covidFile] [queryFile] [populationsFile]")
+    if (args.length != 5) {
+      println("Usage: App [vaccinationsFile] [covidFile] [queryFile] [populationsFile] [queryFile2]")
       return
     }
     System.setProperty("hadoop.home.dir", "c:/winutils/")
@@ -174,10 +174,34 @@ object project {
     )
 
     println(s"K value: $k")
-    println(s"Predicted percentage of population vaccinated: $prediction")
+    println(s"Predicted percentage of population vaccinated: $prediction\n")
 
+    /* Approximate vaccination percentage of the entire world */
     val population_sum = locations.map(location => location._2.population).sum
     val entire_world_prediction = locations.map(location => location._2.percentVaccinated * (location._2.population.toDouble / population_sum)).sum
-    println("Approximate percentage of the entire world vaccinated: %.2f".format(entire_world_prediction))
+    println("Approximate percentage of the entire world vaccinated: %.2f\n".format(entire_world_prediction))
+
+    /* Query2 Case Fatality Rate for given longitude and latitude */
+    val covidReports = sc.textFile(args(1))
+      .map(CovidReport.parse)
+      .zipWithIndex().map(report => (report._2, report._1))
+
+    // A (latitude, longitude) query
+    val query2 = sc.textFile(args(4))
+      .map(line => line.split(", "))
+      .map(line => (line(0).toDouble, line(1).toDouble))
+      .take(1)(0)
+
+    val k_query2 = Math.sqrt(covidReports.count()).toInt
+    // We just send latitude and longitude as the data and caseFatalityRatio
+    val result_case_fatality_rate = knn(
+      covidReports.map(report => (report._1, Array(report._2.latitude, report._2.longitude))),
+      covidReports.map(report => (report._1, Array(report._2.caseFatalityRatio))),
+      Array(query2._1, query2._2),
+      k_query2
+    )
+
+    println(s"K value: $k_query2")
+    println(s"Predicted case fatality ratio: $result_case_fatality_rate\n")
   }
 }
