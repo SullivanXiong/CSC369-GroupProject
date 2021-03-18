0962de80a4d3318a9e288b0d0d8e0fb687881621
Zachary Silver <zmansilver@gmail.com>
2021-03-17 20:33:38 -0700

    updated to include populations data set and predit percentage of population vaccinated given a population size

--- Project/src/main/scala/Project.scala
+++ Project/src/main/scala/Project.scala
@@ -2,6 +2,16 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
+case class Location(country: String,
+                    population: Long,
+                    percentVaccinated: Double)
+
+// This is a concrete representation of select values from
+// a line in a csv file from the COVID-19 World Vaccination Progress dataset
+case class VaccinationReport(country: String,
+                             date: String,
+                             percentVaccinated: Double)
+
 // This is a concrete representation of select values from
 // a line in a csv file from the COVID-19 dataset
 case class CovidReport(country: String,
@@ -61,7 +71,30 @@ object CovidReport {
   }
 }
 
-object Project {
+object VaccinationReport {
+  def toDouble(s: String):Option[Double] = {
+    try {
+      Some(s.toDouble)
+    } catch {
+      case e: NumberFormatException => None
+    }
+  }
+
+  def parse(line: String): VaccinationReport = {
+    // This gross regex is necessary to parse the csv properly
+    val values = line.split(",(?=(?:[^\"]*[\"][^\"]*[\"])*[^\"]*$)")
+
+    // Some values are completely absent from the csv or empty
+    // so we have additional logic to handle those cases safely
+    VaccinationReport(
+      country = values(0).trim,
+      date = values(2).trim,
+      percentVaccinated = toDouble(values(8).trim).getOrElse(0.0)
+    )
+  }
+}
+
+object project {
   // This is how we calculate the euclidean distance between two feature vectors.
   // It's the standard distance formula applied across corresponding features in both vectors.
   def euclideanDistance(a: Array[Double], b: Array[Double]): Double = {
@@ -74,8 +107,8 @@ object Project {
     val nearestNeighbors = data.map(line => {
       (line._1, euclideanDistance(line._2, query))
     })
-      .sortBy(_._2)
       .join(labels)
+      .sortBy(_._2._1)
       .take(k)
 
     val choice = nearestNeighbors.map(_._2._2(0)).sum / nearestNeighbors.length
@@ -84,8 +117,8 @@ object Project {
   }
 
   def main(args: Array[String]): Unit = {
-    if (args.length != 3) {
-      println("Usage: App [vaccinationsFile] [covidFile] [queryFile]")
+    if (args.length != 4) {
+      println("Usage: App [vaccinationsFile] [covidFile] [queryFile] [populationsFile]")
       return
     }
     System.setProperty("hadoop.home.dir", "c:/winutils/")
@@ -95,31 +128,52 @@ object Project {
     val conf = new SparkConf().setAppName("project").setMaster("local[4]")
     val sc = new SparkContext(conf)
 
-    // (lineNumber, CovidReport)
-    // We want to be able to join on line number in our knn function
-    val covidReports = sc.textFile(args(1))
-      .map(CovidReport.parse)
-      .zipWithIndex().map(report => (report._2, report._1))
-
-    // Just a single CovidReport selected directly from the csv for testing purposes
-    val query = sc.textFile(args(2))
-      .map(CovidReport.parse)
-      .take(1)(0)
-
     // Square root of N is often chosen as a good value for K in practice
     //val k = Math.sqrt(covidReports.count()).toInt
     val k = 3
 
-    // We just send latitude and longitude as the data and caseFatalityRatio
-    // as the label we want to predict for testing purposes.
-    val result = knn(
-      covidReports.map(report => (report._1, Array(report._2.latitude, report._2.longitude))),
-      covidReports.map(report => (report._1, Array(report._2.caseFatalityRatio))),
-      Array(query.latitude, query.longitude),
+    // There are multiple reports per country, one per day.
+    // If we want to predict what percentage of the population
+    // should be vaccinated from a given population size,
+    // we get the most recently reported vaccinated percentage
+    // for each country to generate a prediction from.
+    // (vaccinationReport)
+    val vaccinationReports = sc.textFile(args(0))
+      .map(VaccinationReport.parse)
+      .groupBy(_.country)
+      .map(country => country._2.maxBy(_.percentVaccinated))
+
+    // Retrieve populations per country from file.
+    // (country, population)
+    val populations = sc.textFile(args(3))
+      .map(line => line.split(","))
+      .map(line => (line(0).trim, line(1).trim.toLong))
+
+    // Join populations and vaccinationReports on country
+    // and map the results to a Location object containing only what
+    // we really care about. Also zipWithIndex to assigned each location
+    // a unique id as if we were assigning a lineNumber from a file.
+    // (lineNumber, Location)
+    val locations = populations.join(vaccinationReports.keyBy(_.country))
+      .map(country => Location(country._1, country._2._1, country._2._2.percentVaccinated))
+      .zipWithIndex()
+      .map(country => (country._2, country._1))
+
+    // The query file should contain a single integer representing the
+    // desired population the predict.
+    // (population)
+    val query = sc.textFile(args(2))
+      .map(_.trim.toLong)
+      .take(1)(0)
+
+    val prediction = knn(
+      locations.map(location => (location._1, Array(location._2.population))),
+      locations.map(location => (location._1, Array(location._2.percentVaccinated))),
+      Array(query),
       k
     )
 
     println(s"K value: $k")
-    println(s"Predicted case fatality ratio: $result")
+    println(s"Predicted percentage of population vaccinated: $prediction")
   }
 }
